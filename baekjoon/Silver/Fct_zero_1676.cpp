eceec39f07e617a60fcf15a79640dfb5ce8b02e2
Hwan <dnfwlq8054@naver.com>
2021-03-02 17:19:31 +0900

    update

--- baekjoon/Silver/Fct_zero_1676.cpp
+++ baekjoon/Silver/Fct_zero_1676.cpp
@@ -13,17 +13,14 @@
 using namespace std;
 
 int main(){
-    int N, two = 0, five = 0;
+    int N, five = 0;
 
     cin >> N;
-
-    for (int i = 2; i <= N; i *= 2)
-        two += N / i;
 
     for (int i = 5; i <= N; i *= 5)
         five += N / i;
 
-    cout << min(two, five) << endl;
+    cout << five << endl;
 
     return 0;
 }
