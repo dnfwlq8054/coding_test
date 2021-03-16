47b6f0b8fca4228ea175c89650e85bcd0ebce810
Hwan <dnfwlq8054@naver.com>
2021-03-16 21:57:16 +0900

    update

---
+++ baekjoon/Silver/Sum_of_squares_1699.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[100001];
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	dp[0] = 0;
+
+	for (int i = 1; i <= N; i++) {
+	    dp[i] = i;
+        
+		for (int j = 1; j * j <= i; j++) 
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		
+	}
+    
+	cout << dp[N] << endl;
+    
+    return 0;
+}
