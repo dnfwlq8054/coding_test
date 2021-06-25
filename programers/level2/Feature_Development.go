23f02cc391a75f2018a55c35215a4665057719d1
Hwan <dnfwlq8054@naver.com>
2021-06-26 00:07:50 +0900

    update

--- programers/level2/Feature_Development.go
+++ programers/level2/Feature_Development.go
@@ -3,8 +3,8 @@ func solution(progresses []int, speeds []int) []int {
     var max, count int = 0, 0;
     result := []int{};
     
-    for idx, n := range progresses {
-        var remainPeriod int = 100 - n;
+    for idx, completTime := range progresses {
+        var remainPeriod int = 100 - completTime;
         var completPeriod int = (remainPeriod / speeds[idx]);
 
         if ( remainPeriod % speeds[idx] != 0 ) {
